4a5a842841c2af604e08a9d325ddda4d12ab5537
Corey96 <0coreyy@gmail.com>
2022-01-09 22:32:11 +0000

    started html with a button with event click and added countdown timer to js

---
+++ script.js
@@ -0,0 +1,52 @@
+/* 
+
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+1.WHEN I click the start button
+THEN a timer starts and I am presented with a question
+
+2.WHEN I answer a question
+THEN I am presented with another question
+
+3.WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+
+4.WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+
+5.WHEN the game is over
+THEN I can save my initials and my score
+
+*/
+
+// selects element by class
+var timeEl = document.querySelector(".time")
+
+var mainEl =document.getElementById("main")
+var secondsLeft = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " Time Till End";
+
+        if(secondsLeft === 0) [
+            // Stops execution of action at set interval
+            clearInterval(timerInterval),
+            // Calls function to create and append image
+            sendMessage(),
+        ]
+
+    }, 1000);
+}
+
+var score = 0;
+for (var i=0; i < questions.length; i++)
+    var response = window.prompt()
+
+
+<button onclick ="button()">Click Me To Start</button>
+
+object.addEventListener("click", myScript);
