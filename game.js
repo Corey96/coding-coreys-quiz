3e63cfbf4c359bef9fcb730c94604d688d1a8b06
Corey96 <0coreyy@gmail.com>
2022-01-15 22:46:10 +0000

    questions now appearing when press play and switches to next question

---
+++ game.js
@@ -0,0 +1,111 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '5',
+        answer: 4,
+
+},
+{
+        question: 'What is 2 + 5?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '5',
+        answer: 5,
+
+},
+{
+        question: 'What is 21 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '5',
+        answer: 21,
+
+},
+{
+        question: 'What is 0 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '5',
+        answer: 0,
+
+}
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+    
+
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
+
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+    'incorrect'
+
+    if(classToApply === 'correct') {
+        increment(SCORE_POINTS)
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply)
+
+    setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply)
+        getNewQuestion()
+
+    }, 1000)
+})
+})
+
+startGame()
